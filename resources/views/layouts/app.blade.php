d3405a3c5f17b93eaa23bae739c855a3fefaa250
Pedro Falconi <117923037+gitadvauddevp@users.noreply.github.com>
2023-02-14 20:48:25 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <title>Minhas Tarefas</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/layout.css')}}" rel="stylesheet">
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-cinza ">
